fa31ebb3b1b0cc9120d63e47af4777d4aa76405a
Michail Bodzyaev <redekwork@gmail.com>
2023-09-11 12:34:54 +0200

    wtf

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
        
-        System.out.printf("even more text");
+        System.out.printf("even more text12123112312312312312");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
