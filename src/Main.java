d21b5f5ad377a5831675b7ace376db8194dd16ab
redeksan <102988374+redeksan@users.noreply.github.com>
2023-09-11 12:23:38 +0200

    Revert "Merge branch 'master' into expandingmore"
    This reverts commit d0174322a2377b49807940c2468704030639c562.

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-
         System.out.printf("Hello and welcome!");
         System.out.printf("more text");
         System.out.printf("even more text");
-
-        System.out.print("Hello and welcome!");
-
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
